66cf4a18684405cab8e6639149f6e7a052071997
gennadiy <g.ostanin@it-only.ru>
2022-07-13 14:33:46 +0700

    added email scalar

--- tests/django_setup/schema.py
+++ tests/django_setup/schema.py
@@ -3,6 +3,7 @@ from graphene_django_pretty.auth.permissions import AuthenticationRequired
 from graphene_django_pretty.fields.base import BaseField
 from graphene_django_pretty.mutations.base import BaseMutation
 from graphene_django_pretty.mutations.output import BasePayload
+from graphene_django_pretty.scalars.email import Email
 
 
 class Query(graphene.ObjectType):
@@ -24,6 +25,20 @@ class TestInput(graphene.InputObjectType):
     test = graphene.String()
 
 
+class EmailInput(graphene.InputObjectType):
+
+    email = Email()
+
+
+class EmailMutation(BaseMutation):
+    Input = EmailInput()
+    Output = BasePayload
+
+    @classmethod
+    def mutate(cls, info, *args, **kwargs):
+        return cls.Output(message='OK')
+
+
 class Mutation(BaseMutation):
 
     Input = TestInput()
@@ -41,6 +56,7 @@ class LoginRequiredMutation(Mutation):
 class Mutations(graphene.ObjectType):
     mutation = Mutation.Field()
     login_required = LoginRequiredMutation.Field()
+    email = EmailMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
