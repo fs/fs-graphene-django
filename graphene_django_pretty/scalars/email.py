7d6c392122bb965d742428f44500d7def2a602e3
GennadiyOstanin <108727033+GennadiyOstanin@users.noreply.github.com>
2022-07-22 14:41:40 +0300

    added email scalar (#74). Closed #72
    * added email scalar
    
    * DateTime -> Email in Error message
    
    Co-authored-by: gennadiy <g.ostanin@it-only.ru>

---
+++ graphene_django_pretty/scalars/email.py
@@ -0,0 +1,20 @@
+import re
+import graphene
+from graphql import StringValueNode, GraphQLError, print_ast
+
+
+class Email(graphene.Scalar):
+
+    serialize = graphene.String.coerce_string
+    parse_value = graphene.String.coerce_string
+
+    @classmethod
+    def parse_literal(cls, node, _variables=None):
+        pat = r"^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+        if not isinstance(node, StringValueNode):
+            raise GraphQLError(
+                f"Email cannot represent non-string value: {print_ast(node)}"
+            )
+        if not re.match(pat, node.value):
+            raise GraphQLError('Invalid email address.', nodes=[node])
+        return node.value
