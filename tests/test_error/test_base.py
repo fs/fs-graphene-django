c0e626cbeb6440b477d9e3757e2061d29bea1bcb
timuralchin <timur.alchin@flatstack.com>
2022-08-04 11:28:26 +0300

    Fix

--- tests/test_error/test_base.py
+++ tests/test_error/test_base.py
@@ -4,8 +4,11 @@ from graphene_django_pretty.error.exceptions import BaseGraphQLError
 from graphene_django_pretty.error.types import StatusCodeError
 
 
-@pytest.mark.parametrize(
-    ('code', 'message'),
+@pytest.mark.parametrize(  # noqa: WPS317
+    (
+        'code',
+        'message',
+    ),
     [
         (500, 'test error'),
         (None, 'test error'),
@@ -13,7 +16,7 @@ from graphene_django_pretty.error.types import StatusCodeError
         ('ERROR_CODE', 'test error'),
     ],
 )
-def test_valid_errors(code, message):
+def test_valid_error_codes(code, message):
     error = BaseGraphQLError(code=code, message=message)
     assert error.message == message
     assert error.code == code
@@ -21,6 +24,6 @@ def test_valid_errors(code, message):
         assert error.extensions == {'code': code}
 
 
-def test_invalid_error():
+def test_invalid_error_code():
     with pytest.raises(StatusCodeError):
         BaseGraphQLError(code='invalid code')
